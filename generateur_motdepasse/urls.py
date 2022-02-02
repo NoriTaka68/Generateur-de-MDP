babc756c65e0d03359db2bb1d2dd1623f308e2ad
NoriTaka68 <said@evhr.net>
2022-02-02 15:11:33 +0100

    Petite Modif

--- generateur_motdepasse/urls.py
+++ generateur_motdepasse/urls.py
@@ -18,6 +18,7 @@ from generateur import views
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
